34b055d03fd4864a1d4128cb3cc2735cceec0de3
Luiz Rioja <lulyrioja@gmail.com>
2023-03-21 19:51:31 -0300

    style: format files based in the new style guide

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
-    console.log(`Server is running on PORT ${env.PORT} 🚀`);
-  });
+    console.log(`Server is running on PORT ${env.PORT} 🚀`)
+  })
