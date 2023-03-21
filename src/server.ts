56c6a6b49e769c62100819096f99bbae27388ed6
Luiz Rioja <lulyrioja@gmail.com>
2023-03-20 23:24:56 -0300

    refac: load PORT var from env config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { app } from "./app";
+import { env } from "./env";
 
-app.listen({
-  host: "0.0.0.0",
-  port: 3333
-}).then(()=>{
-  console.log(`Server is running on PORT 3333 🚀`);
-  
-})
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`Server is running on PORT ${env.PORT} 🚀`);
+  });
