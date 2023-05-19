664b4d8728dadcf5a074ab6e1168bc616bf44a60
Luiz Rioja <lulyrioja@gmail.com>
2023-05-19 14:15:07 -0300

    feat(createGym): implement useCase

---
+++ src/use-cases/create-gym.ts
@@ -0,0 +1,37 @@
+import { Gym } from '@prisma/client'
+
+import { GymsRepository } from '@/repositories/gyms-repository'
+
+interface CreateGymUseCaseRequest {
+  title: string
+  latitude: number
+  longitude: number
+  phone: string | null
+  description: string | null
+}
+
+interface CreateGymUseCaseResponse {
+  gym: Gym
+}
+
+export class CreateGymUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    title,
+    phone,
+    latitude,
+    longitude,
+    description,
+  }: CreateGymUseCaseRequest): Promise<CreateGymUseCaseResponse> {
+    const gym = await this.gymsRepository.create({
+      title,
+      phone,
+      latitude,
+      longitude,
+      description,
+    })
+
+    return { gym }
+  }
+}
