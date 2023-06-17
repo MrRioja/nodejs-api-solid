54577768ac95f0fac41bc3b0d9cd6d819e440059
Luiz Rioja <lulyrioja@gmail.com>
2023-06-17 14:57:56 -0300

    test(checkIns): add e2e test for create controller

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,38 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Gym 01',
+        latitude: -23.6769801,
+        longitude: -46.6820222,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -23.6769801,
+        longitude: -46.6820222,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
