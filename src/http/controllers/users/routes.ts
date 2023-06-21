8332fcf113e87228256cd6c866eefe8a031b3755
Luiz Rioja <lulyrioja@gmail.com>
2023-06-21 15:00:16 -0300

    feat(users): add route to refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
 import { profile } from './profile'
+import { refresh } from './refresh'
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { verifyJWT } from '../../middlewares/verify-jwt'
@@ -8,5 +9,8 @@ import { verifyJWT } from '../../middlewares/verify-jwt'
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
+
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
